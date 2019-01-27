627f26cde0f47b1645e99a21e59bea04b75f03fa
Mrls94 <sebasmrls94@gmail.com>
2019-01-27 16:02:05 -0500

    Chagne back port

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,4 +4,4 @@ echo 'STOPPING PASSENGER'
 sudo /usr/share/rvm/bin/rvm default do bundle exec passenger stop
 
 echo 'STARTING PASSENGER'
-/usr/share/rvm/bin/rvm default do bundle exec passenger start -d --port 80
+/usr/share/rvm/bin/rvm default do bundle exec passenger start -d
