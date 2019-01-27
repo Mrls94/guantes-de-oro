391f38bf428f91c4f94b5000de822f95d69f16f6
Mrls94 <sebasmrls94@gmail.com>
2019-01-27 15:36:47 -0500

    Change ruby used in scripts

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -xe
 
 echo 'Installing gems'
-/home/ubuntu/.rvm/bin/rvm default do bundle install --deployment
+/usr/share/rvm/bin/rvm default do bundle install --deployment
 
 echo 'Compiling assets'
-/home/ubuntu/.rvm/bin/rvm default do bundle exec rake assets:precompile
+/usr/share/rvm/bin/rvm default do bundle exec rake assets:precompile
 
 echo 'Run migrations'
-/home/ubuntu/.rvm/bin/rvm default do bundle exec rake db:migrate RAILS_ENV=$RAILS_ENV
+/usr/share/rvm/bin/rvm default do bundle exec rake db:migrate RAILS_ENV=$RAILS_ENV
 
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 
 echo 'STOPPING PASSENGER'
-sudo /home/ubuntu/.rvm/bin/rvm default do bundle exec passenger stop
+sudo /usr/share/rvm/bin/rvm default do bundle exec passenger stop
 
 echo 'STARTING PASSENGER'
-/home/ubuntu/.rvm/bin/rvm default do bundle exec passenger start -d
+/usr/share/rvm/bin/rvm default do bundle exec passenger start -d
