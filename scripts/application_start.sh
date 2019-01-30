c3ca3fb6962f9530ffb6f89faab577aee18ba40a
Mrls94 <sebasmrls94@gmail.com>
2019-01-29 21:51:02 -0500

    Add new local

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 
 echo 'STOPPING PASSENGER'
-sudo /usr/share/rvm/bin/rvm default do bundle exec passenger stop -p 3000
+passenger stop -p 3000
 
 echo 'STARTING PASSENGER'
 /usr/share/rvm/bin/rvm default do bundle exec passenger start -d
