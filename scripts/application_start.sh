695e454176332dad7c8da11a01c8f09eeb0efaa3
Mrls94 <sebasmrls94@gmail.com>
2019-01-29 21:12:34 -0500

    Auto load paths

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 
 echo 'STOPPING PASSENGER'
-sudo /usr/share/rvm/bin/rvm default do bundle exec passenger stop
+sudo /usr/share/rvm/bin/rvm default do bundle exec passenger stop -p 3000
 
 echo 'STARTING PASSENGER'
 /usr/share/rvm/bin/rvm default do bundle exec passenger start -d
