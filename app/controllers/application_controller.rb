2de8418aff4373eece77839a87436bb19cb2d346
Mrls94 <sebasmrls94@gmail.com>
2019-01-29 21:58:27 -0500

    Set locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+  
+  def set_locale
+    I18n.locale = :es
+  end
+
   rescue_from CanCan::AccessDenied do |exception|
     respond_to do |format|
       format.json { head :forbidden, content_type: 'text/html' }
